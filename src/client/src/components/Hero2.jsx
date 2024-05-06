4c8175a24baa34c7ca9a1d6f9eebf01bcf8436bb
dscelestial <celestial.danielstephen@gmail.com>
2024-05-06 17:55:11 +0800

    Update Hero section

--- src/client/src/components/Hero2.jsx
+++ src/client/src/components/Hero2.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Hero2 = () => {
     return (
-        <section className='bg-[#051a49] h-40 max-w-screen'>
-            <button className='px-5 py-3 rounded-lg text-white font-semibold bg-[#e60278]'>
-                SEEK
-            </button>
+        <section className='bg-[#051a49] h-40 max-w-screen flex flex-row'>
+
+            <div className='content-center text-center'>
+                <form action="/action_page.php">
+                    <input type="text" id="fname" name="fname" />
+                    <input type="text" id="lname" name="lname" />
+                    <input type="submit" defaultValue="Submit" />
+                </form>
+            </div>
+
+            <div className='text-center content-center'>
+                <button className='px-5 py-3 rounded-lg text-white font-semibold bg-[#e60278]'>
+                    SEEK
+                </button>
+            </div>
         </section>
     )
 }
